6243cdd76cb762697d273ef45a301f72f7abe7db
devdussey <ukdussey@hotmail.com>
2025-11-29 13:34:59 +0000

    Clean rebuild - remove old Supabase references

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,55 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as auth from "../auth.js";
+import type * as helpers_lobbyCode from "../helpers/lobbyCode.js";
+import type * as helpers_password from "../helpers/password.js";
+import type * as lobbies from "../lobbies.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
+  "helpers/lobbyCode": typeof helpers_lobbyCode;
+  "helpers/password": typeof helpers_password;
+  lobbies: typeof lobbies;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
