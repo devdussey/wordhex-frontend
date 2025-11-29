2e0be15e3511a965dd4d49ba4a9adad06b6a4ffa
devdussey <ukdussey@hotmail.com>
2025-11-29 07:59:44 +0000

    Redirect home page to lobby

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 
-export default function Home(){
-  return (
-    <main className='p-6'>
-      <h1 className='text-3xl font-bold'>WordHex Build Base</h1>
-      <p className='text-gray-300 mt-2'>The frontend base is installed successfully.</p>
-    </main>
-  );
+import { redirect } from 'next/navigation';
+
+export default function Home() {
+  redirect('/lobby');
 }
